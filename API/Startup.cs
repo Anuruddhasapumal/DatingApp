5527f006df88cd6c6dde581e96a0f83f6a344238
Anuruddhasapumal <sapumalanuruddha@gmail.com>
2021-06-06 13:06:37 +0800

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,14 +18,14 @@ namespace API
 {
     public class Startup
     {
-        public IConfiguration _config ;
+        public IConfiguration _config;
         public Startup(IConfiguration config)
         {
             _config = config;
-            
+
         }
 
-        
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -37,6 +37,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -56,7 +57,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().
+            WithOrigins("http://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
